b31f85d060f62b5ce39c9b4ca1df52e5535ffec1
BRIAN ROBINSON <1008361743@mwce111-14.local>
2020-01-23 18:09:45 -0800

    Initial Commit

---
+++ CIT238B/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CIT238B
+//
+//  Created by BRIAN ROBINSON on 1/23/20.
+//  Copyright © 2020 BRIAN ROBINSON. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
